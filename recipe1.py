ab110317fed29e1252153efb0cee891653a1d74a
Phoebe Ireland <pireland2016@gmail.com>
2024-03-19 14:44:58 -0400

    add code to fix the comma separation in the csv file

--- recipe1.py
+++ recipe1.py
@@ -3,9 +3,8 @@ import random
 
 def get_random_recipe(csv_file):
     with open(csv_file, newline='') as file:
-        reader = csv.reader(file)
+        reader = csv.reader(file, delimiter=';')
         next(reader)  # Skip header 1
-        next(reader)  # Skip header 2
         recipes = list(reader)
         # Choose a random recipe
         random_recipe = random.choice(recipes)
